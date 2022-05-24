1699587c26298e1eff955fd5940525c71416c485
Ravi Gola <golaravi555@gmail.com>
2022-05-24 16:57:47 +0530

    some changes done in my project for deployment

--- prajapatiblog/settings.py
+++ prajapatiblog/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z*$4xcre&1=+1x&c!+_vjexlfy%865odtnya7e&8mfcrp=d6g#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- prajapatiblog/urls.py
+++ prajapatiblog/urls.py
@@ -13,14 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from . import views
 admin.site.site_header="PrajapatiBlog"
 admin.site.site_title="PrajapatiBlog panel"
 admin.site.index_title="Welcome to PrajapatiBlog Admin Panel"
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include('blog.urls'))
+    path("",include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
